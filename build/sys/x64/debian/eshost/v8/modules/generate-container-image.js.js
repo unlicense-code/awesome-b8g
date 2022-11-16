300ae2b92d2d233f39257249a33162191ca73430
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-16 07:56:20 +0100

    Create generate-container-image.js.js

---
+++ build/sys/x64/debian/eshost/v8/modules/generate-container-image.js.js
@@ -0,0 +1,3 @@
+const const { content: { "x64": { "debian": { "v8": { Dockerfile } } = import('dirs/build.js');
+export const generateBundle = () => (await import('/sys/container/docker')).run(Dockerfile);
+

3c315d9400735f3a9b39652aa7679c6f34ee0ae0
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-16 08:58:32 +0100

    Update isolate.js

--- build/isolate/isolate.js
+++ build/isolate/isolate.js
@@ -1,1 +1,7 @@
+// the isolate component/module returns a context so it is like 
+// the vm module in nodejs.
 // Build Instructions for Isolate and a basic main
+const build = {
+  "x64/debian/graaljs": "Dockerfile" 
+  "x64/debian/v8": "Dockerfile" 
+}

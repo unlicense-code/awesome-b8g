9233146f6b7877b1303e68c7f0946cb109133f56
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-16 07:34:03 +0100

    Update and rename build/obj/SYS_BOOT::sysroots.js to build/sys/x64/debian/eshost/v8/10.8/modules/dirs/build.js

--- build/sys/x64/debian/eshost/v8/10.8/modules/dirs/build.js
+++ build/sys/x64/debian/eshost/v8/10.8/modules/dirs/build.js
@@ -1,8 +1,22 @@
-// sysroots["bullseye-x64"].v8.Dockerfile
-const meta = ['bullseye-x64',['v8/Dockerfile']];
-const sysroots = [{ "bullseye-x64": { 
-  "v8": {
-    "Dockerfile": `# Note that this is exactly what docker build does for each RUN defined layer
+// As convention the propertys are functions we call them and await the result
+export const meta = { 
+	// As a convention we always return the result of readdir(path, { rescursive: true });
+	readdir: () => ['debian-x64',['v8/Dockerfile']],
+	// ('debian-x64/v8/Dockerfile')
+	readFile: (path) => path.split('/').reduce((content, key) => content = content[key], content);
+}
+// The Prefered way to access content is to simply iterate over it while meta should supply lazyHelpers
+// Should also use modules MODULE::READFILE prefixed keys indicate a Useable Module 
+// content["debian-x64"].v8.Dockerfile
+export const content = [{ "debian-x64": { "v8": {
+    // Highly Annotated Docker File that includes a lot of posix magic by intent to teach you 
+    // Importent concepts for porting software that needs a specific OS to build properly.
+    // This should later better get a referenced Object like BUILD/DOCKER::SYS::x64::DEBIAN::10.8.xxx::SHA256.js To Reflect better what that builds
+    // NOTE: about keys SHASUM needs to get added to the end to be strip able easy in forward search but be there in case of static ref.
+    // As OUR BASE IS V8 only the DEBIAN Note is meaningfull as it refers to debian used to build v8 with our deps. also CPU Arch matters.
+    // Result: SYS::x64::DEBIAN::10.8.xxx::SHA256.blob SYS::x64::V8::SHA256.blob v8-x64-SHA256.10.8.xxx.blob
+    "Dockerfile": `# cat <<'SHA_SUM_CREATION' | sha256sum
+# Note that this is exactly what docker build does for each RUN defined layer
 # cat <<'EOF' | docker run -i --name sysroot-bullseye-x86_64 debian:bullseye-slim
 FROM debian:bullseye-slim
 RUN export DEBIAN_FRONTEND=noninteractive \
@@ -62,6 +76,7 @@ RUN /depot_tools/ninja v8_monolith -C out.gn/x64.release/ -j $(getconf _NPROCESS
 # Output: v8-bullseye-x86_64:latest === This Dockerfiles Result
 
 # docker save v8-bullseye-x86_64:latest | gzip > v8-bullseye-x86_64-latest.tar.gz
-# above created tar includes 2 seperated tar files for the images one for sysroot one for build result.`,
+# above created tar includes 2 seperated tar files for the images one for sysroot one for build result.
+# 8f5848ef193c85cc74abe276b8be2d88a94eb88523a69b30ddbe681f32ff98a6 SHA_SUM_CREATION`,
   }
 } }];

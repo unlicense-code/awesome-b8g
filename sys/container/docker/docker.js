31fbc0bb6ad697f17533293704bfbc1d4b39997e
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-16 07:58:49 +0100

    Create docker.js

---
+++ sys/container/docker/docker.js
@@ -0,0 +1,3 @@
+// As a convention we add directory import support always referencing the corrsoponding js file
+// in this case the resolved result is /sys/container/docker/docker.js
+// import('/sys/container/docker/') 

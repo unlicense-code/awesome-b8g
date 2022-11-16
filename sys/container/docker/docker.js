b359bec7dd44e7762b95fabb519aaa14ef518e64
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-16 08:04:34 +0100

    Update docker.js

--- sys/container/docker/docker.js
+++ sys/container/docker/docker.js
@@ -1,3 +1,6 @@
 // As a convention we add directory import support always referencing the corrsoponding js file
 // in this case the resolved result is /sys/container/docker/docker.js
-// import('/sys/container/docker/') 
+// in case of binary data you should simply return obj describing how to handle the binary data like readAsText, readAsBlob or .text .blob
+// its also a convention to return iterables directly or functions that return them. It is then up to the importer to choose the right method.
+// for the imported object mostly the d.ts files give type hints..
+// import('awesome:/sys/container/docker/');

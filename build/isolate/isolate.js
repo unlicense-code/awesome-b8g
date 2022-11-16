5ce1fd18b3519994ac93c4609e71137dbcb4a12e
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-16 09:16:29 +0100

    Update and rename build/isolate/isolate.js to build/isolate/platform/x64.js

--- build/isolate/isolate.js
+++
@@ -1,7 +0,0 @@
-// the isolate component/module returns a context so it is like 
-// the vm module in nodejs.
-// Build Instructions for Isolate and a basic main
-const build = {
-  "x64/debian/graaljs": "Dockerfile" 
-  "x64/debian/v8": "Dockerfile" 
-}

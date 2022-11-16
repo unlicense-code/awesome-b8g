31e4f238a8e9a85b542ac5362fa88321a2f9010b
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-11-16 09:35:27 +0100

    Create isolate.js

---
+++ build/isolate/isolate.js
@@ -0,0 +1,2 @@
+// Build instructions that use ./platform to build isolate and corrosponding files if needed. maybe we can combine some build steps for all 
+// modules into a set of simple instructions. that we then let create a vfs. and run into a compiler as the most are anyway file driven.
